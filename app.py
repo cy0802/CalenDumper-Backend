dda8d24db9ff0652e3c926c9854d038d3f8ddcdc
cy0802 <amyyoyo0802@gmail.com>
2024-10-19 09:54:17 +0800

    service layer

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, redirect, session, request
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
+from flask import Flask, redirect, request
+from services.googleoauth import authorize, oauth_callback, refresh
 from dotenv import load_dotenv
 import os
 
@@ -8,11 +7,9 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('APP_SECRET', 'default_fallback_secret_key')
+frontend_url = os.getenv('FRONTEND_URL', 'http://localhost:3000')
 
 scopes = ['https://www.googleapis.com/auth/calendar.events.owned.readonly']
-cred = "credential.json"
-redirect_uri = "http://localhost:8000/callback"
-
 
 @app.route("/")
 def hello():
@@ -20,26 +17,13 @@ def hello():
 
 @app.route("/login")
 def login():
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file (
-        cred, scopes=scopes)
-    flow.redirect_uri = redirect_uri
-    authorization_url, state = flow.authorization_url (
-        access_type='offline',
-        include_granted_scopes='true')
-    session['state'] = state
+    authorization_url = authorize(scopes)
     return redirect(authorization_url)
 
 @app.route("/callback")
 def callback():
-    state = session.get('state')
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        cred, scopes=scopes, state=state)
-    flow.redirect_uri = redirect_uri
-    if state != request.args.get('state'):
-        return "Error: State mismatch. Possible CSRF attack.", 400
-    flow.fetch_token(authorization_response=request.url)
-    credentials = flow.credentials
-    return f"Access Token: {credentials.token}"
+    access_token = oauth_callback(scopes)
+    return redirect(frontend_url)
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
---
+++ services/googleoauth.py
@@ -0,0 +1,31 @@
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+from flask import session, request
+
+# scopes = ['https://www.googleapis.com/auth/calendar.events.owned.readonly']
+cred = "credential.json"
+redirect_uri = "http://localhost:8000/callback"
+
+def authorize(scopes):
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file (
+        cred, scopes=scopes)
+    flow.redirect_uri = redirect_uri
+    authorization_url, state = flow.authorization_url (
+        access_type='offline',
+        include_granted_scopes='true')
+    session['state'] = state
+    return authorization_url
+
+def oauth_callback(scopes):
+    state = session.get('state')
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        cred, scopes=scopes, state=state)
+    flow.redirect_uri = redirect_uri
+    if state != request.args.get('state'):
+        return "Error: State mismatch. Possible CSRF attack.", 400
+    flow.fetch_token(authorization_response=request.url)
+    credentials = flow.credentials
+    return credentials.token
+
+def refresh():
+    pass
