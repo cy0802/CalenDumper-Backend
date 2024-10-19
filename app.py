28cf3ccc6d10c5c8dea1a44cc7b7c7dc7e8ec69a
cy0802 <amyyoyo0802@gmail.com>
2024-10-19 23:12:40 +0800

    save note and picture

--- app.py
+++ app.py
@@ -7,18 +7,29 @@ import jwt
 from models import db, User, Note, Dump
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from functools import wraps
+from werkzeug.utils import secure_filename
+from datetime import datetime
 
 load_dotenv()
 
+UPLOAD_FOLDER = 'picture/'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = os.getenv('APP_SECRET', 'default_fallback_secret_key')
 frontend_url = os.getenv('FRONTEND_URL', 'http://localhost:3000')
+backend_url = os.getenv('BACKEND_URL', 'http://localhost:8000')
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///default.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class User_Login(UserMixin):
     def __init__(self, email, access_token):
@@ -100,23 +111,24 @@ def hello():
 
 @app.route("/login", methods=['GET'])
 def login():
-    authorization_url = authorize(scopes)
+    authorization_url = authorize(scopes, f"{backend_url}/callback")
     return redirect(authorization_url)
 
 @app.route("/callback")
 def callback():
-    access_token = oauth_callback(scopes)
+    access_token = oauth_callback(scopes, f"{backend_url}/callback")
     # query userinfo
     userinfo = get_userinfo(access_token)
+    print(userinfo)
     
     # login user
     global users
     users.update({userinfo['email']: User_Login(userinfo['email'], access_token)})
     login_user(users[userinfo['email']])
 
-    return redirect(f"{frontend_url}?token={users[userinfo['email']].generate_token()}")
+    return redirect(f"{frontend_url}/?token={users[userinfo['email']].generate_token()}")
 
-@app.route("/event/<date>", methods=['GET'])
+@app.route("/api/event/<date>", methods=['GET'])
 @token_required
 def events(date):
     token = request.headers.get('Authorization')
@@ -127,9 +139,80 @@ def events(date):
         'start_time': event.event_start,
         'end_time': event.event_end,
         'title': event.event_summary,
-        'text': event.text,
+        'note': event.text,
         'picture': event.picture
     } for event in events])
+
+@app.route("/api/event/<event_id>/note", methods=['POST'])
+@token_required
+def add_note(event_id):
+    token = request.headers.get('Authorization')
+    user = get_current_user(token)
+    event = Note.query.filter_by(event_id=event_id).first()
+    if not event:
+        return jsonify({'message': 'Event not found!'}), 404
+    data = request.json
+    event.text = data.get('note', '')
+    db.session.commit()
+    return jsonify({'message': 'Note added'})
+
+@app.route("/api/event/<event_id>/picture", methods=['POST'])
+@token_required
+def add_picture(event_id):
+    token = request.headers.get('Authorization')
+    user = get_current_user(token)
+    event = Note.query.filter_by(event_id=event_id).first()
+    if not event:
+        return jsonify({'message': 'Event not found!'}), 404
+    if event.picture != '':
+        return jsonify({'message': 'Picture already exists'}), 400
+    if 'file' not in request.files:
+        return jsonify({'message': 'No file part'}), 400
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+    
+    if file and allowed_file(file.filename):
+        # Use secure_filename to sanitize the filename
+        event_summary = secure_filename(event.event_summary)
+        file_extension = file.filename.rsplit('.', 1)[1].lower()
+        filename = f"{event_id}_{event_summary}.{file_extension}"
+        
+        # Save the file to the specified folder
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        # Save the picture path to the event (assuming there is a picture field in the Note model)
+        event.picture = filename
+        db.session.commit()
+        
+        return jsonify({'message': 'Picture added successfully', 'filename': filename}), 200
+    else:
+        return jsonify({'message': 'Invalid file type. Allowed types: png, jpg, jpeg, gif'}), 400
+
+# @app.route("/api/event", methods=['POST'])
+# @token_required
+# def add_event():
+#     token = request.headers.get('Authorization')
+#     user = get_current_user(token)
+#     data = request.json
+#     to_calandar = data['to_calendar']
+#     event_id = hash((user.id, data['title'], data['start_time'], data['end_time']))
+#     event_start = datetime.strptime(data['start_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+#     event_end = datetime.strptime(data['end_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+#     event = Note(
+#         user_id=user.id,
+#         event_id=event_id,
+#         event_start=event_start,
+#         event_end=event_end,
+#         event_summary=data['title'],
+#         text='',
+#         picture=''
+#     )
+#     db.session.add(event)
+#     db.session.commit()
+#     # TODO: if to_calendar is True, add the event to the user's calendar
+#     return jsonify({'message': 'Event added'})
 
 if __name__ == "__main__":
     with app.app_context():
--- services/googleoauth.py
+++ services/googleoauth.py
@@ -5,9 +5,8 @@ import requests
 
 # scopes = ['https://www.googleapis.com/auth/calendar.events.owned.readonly']
 cred = "credential.json"
-redirect_uri = "http://localhost:8000/callback"
 
-def authorize(scopes):
+def authorize(scopes, redirect_uri):
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file (
         cred, scopes=scopes
     )
@@ -17,9 +16,10 @@ def authorize(scopes):
         include_granted_scopes='true'
     )
     session['state'] = state
+    print(authorization_url)
     return authorization_url
 
-def oauth_callback(scopes):
+def oauth_callback(scopes, redirect_uri):
     state = session.get('state')
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         cred, scopes=scopes, state=state)
